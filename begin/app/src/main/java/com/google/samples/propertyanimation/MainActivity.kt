75b1fb5b85c6c7902f15e04751dc03641b79bea1
Dandan Meng <mengdandanno1@163.com>
2019-12-24 08:42:28 +0800

    Lesson 6-7: use ObjectAnimator.ofInt() to animate color

--- begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -20,6 +20,7 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -122,6 +123,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun colorizer() {
+        var animator =
+            ObjectAnimator.ofInt(star.parent, "backgroundColor", Color.BLACK, Color.RED).start()
     }
 
     private fun shower() {
